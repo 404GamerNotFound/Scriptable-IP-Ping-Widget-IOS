21f5090af05b028778a1e37217ce2e87ff308c3f
Tony Brüser <tonybrueser.1995@gmail.com>
2023-11-26 15:16:14 +0100

    added Ping Historie for large widget

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 const ip = args.widgetParameter || "http-statuscode.com"; // ADD IP OR DOMAIN as Widget Parameter
 const url = `http://${ip}`;
+const fileManager = FileManager.local();
+const historyFilePath = fileManager.joinPath(fileManager.documentsDirectory(), 'ping_history.json');
+
+// Funktion zum Speichern der Ping-Daten
+async function savePingData(pingData) {
+    let historyData = [];
+    if (fileManager.fileExists(historyFilePath)) {
+        const rawHistoryData = fileManager.readString(historyFilePath);
+        historyData = JSON.parse(rawHistoryData);
+    }
+    historyData.push(pingData);
+    // Alte Daten entfernen, um die Dateigröße zu begrenzen
+    if (historyData.length > 50) { // Behält nur die letzten 50 Einträge
+        historyData = historyData.slice(-50);
+    }
+    fileManager.writeString(historyFilePath, JSON.stringify(historyData));
+}
+
+// Funktion zum Lesen der gespeicherten Ping-Daten
+function readPingHistory() {
+    if (fileManager.fileExists(historyFilePath)) {
+        const rawHistoryData = fileManager.readString(historyFilePath);
+        return JSON.parse(rawHistoryData);
+    } else {
+        return [];
+    }
+}
+
+function addHistoryEntry(column, data) {
+    const pingColor = data.ping > 500 ? Color.red() : (data.ping > 200 ? Color.yellow() : Color.green());
+    const formattedDate = new Date(data.timestamp).toLocaleString(); // Datum im lokalen Format
+    
+    let historyText = column.addText(`Ping: ${data.ping} ms at ${formattedDate}`);
+    historyText.textColor = pingColor;
+    historyText.font = Font.systemFont(10);
+}
+
+
+
 
 async function pingServer() {
     const start = new Date().getTime();
@@ -23,74 +62,164 @@ function formatTime(date) {
     let hours = date.getHours();
     let minutes = date.getMinutes().toString().padStart(2, '0');
     let seconds = date.getSeconds().toString().padStart(2, '0');
-
+    
     if (hours === 0) {
         hours = 12;
     } else if (hours > 12) {
         hours = hours - 12;
         ampm = "PM";
     }
-
+    
     return `${hours}:${minutes}:${seconds} ${ampm}`;
 }
 
 async function createWidget(result, size = "small") {
     let widget = new ListWidget();
     widget.backgroundColor = new Color("#1A1A1A");
-
+    
     if (size === "medium") {
         let mainStack = widget.addStack();
-    mainStack.layoutHorizontally();
-
-    // Left side - Large Ping
-    let pingStack = mainStack.addStack();
-    pingStack.layoutVertically();
-    pingStack.centerAlignContent();
-    pingStack.size = new Size(150, 0); // Set a fixed width for the pingStack
-
-    // Ping Value
-    let pingValue = pingStack.addText(`${result.ping}`);
-    pingValue.textColor = Color.green();
-    pingValue.font = Font.boldSystemFont(60);
-    pingValue.textOpacity = 1.0;
-
-    // Ping Unit
-    let pingUnit = pingStack.addText(`ms`);
-    pingUnit.textColor = Color.green();
-    pingUnit.font = Font.systemFont(16);
-    pingUnit.textOpacity = 0.8;
-
-    mainStack.addSpacer();
-
-    // Right side - Details
-    let detailsStack = mainStack.addStack();
-    detailsStack.layoutVertically();
-    detailsStack.spacing = 4;
-    detailsStack.size = new Size(150, 0); // Set a fixed width for the detailsStack
-
-let title = detailsStack.addText("Server Ping");
+        mainStack.layoutHorizontally();
+        
+        // Left side - Large Ping
+        // Ping Stack with centered content
+        let pingStack = mainStack.addStack();
+        pingStack.layoutVertically();
+        pingStack.centerAlignContent();  // Diese Zeile zentriert den Inhalt des pingStack
+        pingStack.size = new Size(150, 0); // Set a fixed width for the pingStack
+        
+        // Ping Value
+        let pingValue = pingStack.addText(`${result.ping}`);
+        pingValue.textColor = Color.green();
+        pingValue.font = Font.boldSystemFont(60);
+        pingValue.textOpacity = 1.0;
+        
+        // Ping Unit
+        let pingUnit = pingStack.addText(`ms`);
+        pingUnit.textColor = Color.green();
+        pingUnit.font = Font.systemFont(16);
+        pingUnit.textOpacity = 0.8;
+        
+        mainStack.addSpacer();
+        
+        // Right side - Detail
+        let detailsStack = mainStack.addStack();
+        detailsStack.layoutVertically();
+        detailsStack.spacing = 4;
+        detailsStack.size = new Size(150, 0); // Set a fixed width for the detailsStack
+        
+        let title = detailsStack.addText("Server Ping");
         title.textColor = Color.white();
         title.font = Font.boldSystemFont(20);
         
         widget.addSpacer(9);
-
-
+        
+        
         let pingTextIp = detailsStack.addText(`${url}`);
         pingTextIp.textColor = Color.white();
         pingTextIp.font = Font.systemFont(8);
-
+        
         let statusText = detailsStack.addText(`Status: ${result.statusCode}`);
         statusText.textColor = Color.white();
         statusText.font = Font.systemFont(12);
-
+        
         let sizeText = detailsStack.addText(`Size: ${result.dataSize} Zeichen`);
         sizeText.textColor = Color.white();
         sizeText.font = Font.systemFont(12);
-
+        
         const updateTime = new Date();
         let updateText = detailsStack.addText(`Updated: ${formatTime(updateTime)}`);
         updateText.textColor = Color.gray();
         updateText.font = Font.systemFont(12);
+    } else if (size === "large") {
+        let mainStack = widget.addStack();
+        mainStack.layoutHorizontally();
+        
+        // Left side - Large Ping
+        // Ping Stack with centered content
+        let pingStack = mainStack.addStack();
+        pingStack.layoutVertically();
+        pingStack.centerAlignContent();  // Diese Zeile zentriert den Inhalt des pingStack
+        pingStack.size = new Size(150, 0); // Set a fixed width for the pingStack
+        
+        // Ping Value
+        let pingValue = pingStack.addText(`${result.ping}`);
+        pingValue.textColor = Color.green();
+        pingValue.font = Font.boldSystemFont(60);
+        pingValue.textOpacity = 1.0;
+        
+        // Ping Unit
+        let pingUnit = pingStack.addText(`ms`);
+        pingUnit.textColor = Color.green();
+        pingUnit.font = Font.systemFont(16);
+        pingUnit.textOpacity = 0.8;
+        
+        mainStack.addSpacer();
+        
+        // Right side - Detail
+        let detailsStack = mainStack.addStack();
+        detailsStack.layoutVertically();
+        detailsStack.spacing = 4;
+        detailsStack.size = new Size(150, 0); // Set a fixed width for the detailsStack
+        
+        let title = detailsStack.addText("Server Ping");
+        title.textColor = Color.white();
+        title.font = Font.boldSystemFont(20);
+        
+        widget.addSpacer(9);
+        
+        
+        let pingTextIp = detailsStack.addText(`${url}`);
+        pingTextIp.textColor = Color.white();
+        pingTextIp.font = Font.systemFont(8);
+        
+        let statusText = detailsStack.addText(`Status: ${result.statusCode}`);
+        statusText.textColor = Color.white();
+        statusText.font = Font.systemFont(12);
+        
+        let sizeText = detailsStack.addText(`Size: ${result.dataSize} Zeichen`);
+        sizeText.textColor = Color.white();
+        sizeText.font = Font.systemFont(12);
+        
+        const updateTime = new Date();
+        let updateText = detailsStack.addText(`Updated: ${formatTime(updateTime)}`);
+        updateText.textColor = Color.gray();
+        updateText.font = Font.systemFont(12);
+        
+        // Trennlinie oder Spacer
+        widget.addSpacer();
+        
+        // Historie-Stack
+        // Historie-Stack
+        let historyStack = widget.addStack();
+        historyStack.layoutHorizontally();
+        
+        // Berechnen der Breite für jede Spalte
+        let columnWidth = 150; // Beispielbreite, passen Sie sie nach Bedarf an
+        
+        let leftColumn = historyStack.addStack();
+        leftColumn.layoutVertically();
+        leftColumn.size = new Size(columnWidth, 0);
+        
+        historyStack.addSpacer(); // Fügt einen Spacer zwischen den Spalten hinzu
+        
+        let rightColumn = historyStack.addStack();
+        rightColumn.layoutVertically();
+        rightColumn.size = new Size(columnWidth, 0);
+        
+        
+        // Verteilung der Historiendaten auf die Spalten
+        const historyData = readPingHistory();
+        const halfLength = Math.ceil(historyData.length / 2);
+        
+        for (let i = 0; i < halfLength; i++) {
+            addHistoryEntry(leftColumn, historyData[i]);
+        }
+        
+        for (let i = halfLength; i < historyData.length; i++) {
+            addHistoryEntry(rightColumn, historyData[i]);
+        }
+        
     } else {
         // Title
         let title = widget.addText("Server Ping");
@@ -98,44 +227,48 @@ let title = detailsStack.addText("Server Ping");
         title.font = Font.boldSystemFont(16);
         
         widget.addSpacer(5);
-
+        
         // URL
         let pingTextIp = widget.addText(`${url}`);
         pingTextIp.textColor = Color.white();
         pingTextIp.font = Font.systemFont(8);
-
+        
         widget.addSpacer(5);
-
+        
         // Ping
         let pingText = widget.addText(`${result.ping} ms`);
         pingText.textColor = Color.green();
         pingText.font = Font.systemFont(32);
-
+        
         widget.addSpacer(5);
-
+        
         // Status and Size
         let statusText = widget.addText(`Status: ${result.statusCode} \nSize: ${result.dataSize} Zeichen`);
         statusText.textColor = Color.white();
         statusText.font = Font.systemFont(12);
-
+        
         widget.addSpacer(5);
-
+        
         // Updated Time
         const updateTime = new Date();
         let updateText = widget.addText(`Updated: ${formatTime(updateTime)}`);
         updateText.textColor = Color.gray();
         updateText.font = Font.systemFont(12);
-
+        
     }
-
+    
     return widget;
 }
 
 async function main() {
     const result = await pingServer();
+    await savePingData({ ping: result.ping, timestamp: new Date().toISOString() }); // Speichern mit Zeitstempel
+    
     let widget;
     if (config.widgetFamily === "medium") {
         widget = await createWidget(result, "medium");
+    } else if (config.widgetFamily === "large") {
+        widget = await createWidget(result, "large");
     } else {
         widget = await createWidget(result, "small");
     }
